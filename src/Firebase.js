8aaf17503c9988026eaf29de9bee4dc914f327fa
Titoo <title.spp@gmail.com>
2019-09-11 22:42:37 +0700

    add some dependencies

---
+++ src/Firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyAQylxiRhe28x39ygCfFCdy8YuLjE6_5Do',
+  authDomain: 'larngearcamp19th.firebaseapp.com',
+  databaseURL: 'https://larngearcamp19th.firebaseio.com',
+  projectId: 'larngearcamp19th',
+  storageBucket: 'larngearcamp19th.appspot.com',
+  messagingSenderId: '3832635855',
+  appId: '1:3832635855:web:35beb3f83be8d662'
+};
+firebase.initializeApp(config);
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { db, auth, storage };
