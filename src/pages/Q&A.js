ba953dfcd7eb9d40e43411165ba8fd9d9bdd13bc
Titoo <title.spp@gmail.com>
2019-09-19 01:37:18 +0700

    fix Q&A

--- src/pages/Q&A.js
+++ src/pages/Q&A.js
@@ -4,9 +4,9 @@ function QandAPage() {
   return (
     <React.Fragment>
       <div className="q-a-page">
-
-      <div className = "title">
-        <h1>Frequently Asked Questions</h1>
+        <div className="title">
+          <h1>Frequently Asked Questions</h1>
+        </div>
       </div>
     </React.Fragment>
   );
